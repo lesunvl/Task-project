b738561aae08aeb5eef57953129ca33e633a64fa
Vlad Lesun <vladlesun@vlads-mbp.home>
2021-01-24 13:58:04 -0500

    commit eleven

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Its me");
 
 
     }
