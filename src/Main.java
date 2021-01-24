d258e46db9da29d19d1545280cd7edcfcd61f68c
Vlad Lesun <vladlesun@vlads-mbp.home>
2021-01-24 13:31:45 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Its me");
-        System.out.println("How are you");
+
 
     }
 }
